e25d3e586e64a7322db6e8d0b247b492c471c7d7
jakkubu <jakkubu@gmail.com>
2016-03-09 15:01:04 +0100

    name confilcts bux fix

--- ansi.py
+++ ansi.py
@@ -78,8 +78,8 @@ class AnsiEventListener(sublime_plugin.EventListener):
 
 class AnsiColorBuildCommand(Default.exec.ExecCommand):
 
-    on_data = False
-    on_finish = True
+    process_on_data = False
+    pricess_on_finish = True
 
     @classmethod
     def update_build_settings(cls):
@@ -87,11 +87,11 @@ class AnsiColorBuildCommand(Default.exec.ExecCommand):
         settings = sublime.load_settings("ansi.sublime-settings")
         val = settings.get("ANSI_process_trigger", "on_finish")
         if val == "on_finish":
-            cls.on_data = False
-            cls.on_finish = True
+            cls.process_on_data = False
+            cls.pricess_on_finish = True
         elif val == "on_data":
-            cls.on_data = True
-            cls.on_finish = False
+            cls.process_on_data = True
+            cls.pricess_on_finish = False
 
     def process_ansi(self):
         view = self.output_view
@@ -102,12 +102,12 @@ class AnsiColorBuildCommand(Default.exec.ExecCommand):
 
     def on_data(self, proc, data):
         super(AnsiColorBuildCommand, self).on_data(proc, data)
-        if self.on_data:
+        if self.process_on_data:
             self.process_ansi()
 
     def on_finished(self, proc):
         super(AnsiColorBuildCommand, self).on_finished(proc)
-        if self.finish:
+        if self.pricess_on_finish:
             self.process_ansi()
 
 
